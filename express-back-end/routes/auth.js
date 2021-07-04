b40ff4567c79db7155b607fb21d464e27435edd4
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-04 04:04:50 +0000

    combine login and register into 1 auth route

--- express-back-end/routes/auth.js
+++ express-back-end/routes/auth.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
+  router.post('/', (req, res) => {
     res.json({ ratings: 'Works' });
   });
   router.get('/:id', (req, res) => {
     res.json({ ratings: 'Works' });
   });
-
   return router;
 };
