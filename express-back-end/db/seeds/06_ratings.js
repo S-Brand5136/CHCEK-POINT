ddd23ff409f80293573d696562d43afa3431575a
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-04 03:14:04 +0000

    fix typo

--- express-back-end/db/seeds/06_ratings.js
+++ express-back-end/db/seeds/06_ratings.js
@@ -3,7 +3,7 @@ exports.seed = function (knex) {
   return knex('ratings')
     .del()
     .then(function () {
-      return knex('tags').insert([
+      return knex('ratings').insert([
         { user_id: 1, game_id: 5, rating: 25 },
         { user_id: 2, game_id: 3, rating: 100 },
         { user_id: 3, game_id: 84, rating: 64 },
