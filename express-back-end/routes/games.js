db1beab9b552371a9097c9de3a50d956b9c9a7f6
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-04 04:16:12 +0000

    refactor games/:id route to sum total rating and get each tag_name from tags table

--- express-back-end/routes/games.js
+++ express-back-end/routes/games.js
@@ -21,20 +21,24 @@ module.exports = (db) => {
       });
   });
   // GET: A specific game from the catalog with its ratings and tags
-
-  // TODO: Fix the outerJoin from tags
-
   router.get('/:id', (req, res) => {
     db('games_catalog')
-      .select('games_catalog.*', 'ratings.rating', 'tags.tag_name')
+      .select('games_catalog.*', 'ratings.rating')
+      .sum('ratings.rating')
       .where({ 'games_catalog.id': req.params.id })
       .leftOuterJoin('ratings', 'games_catalog.id', '=', 'ratings.game_id')
-      .leftOuterJoin('tags', 'tags.game_id', '=', 'games_catalog.id')
+      .groupBy('games_catalog.id', 'ratings.rating')
       .then((game) => {
-        if (game.length > 0) {
-          return res.status(200).json({ game });
-        }
-        throw Error;
+        db('tags')
+          .select('tag_name')
+          .where({ game_id: req.params.id })
+          .groupBy('tag_name')
+          .then((tag_list) => {
+            if (game.length > 0) {
+              return res.status(200).json({ game, tag_list });
+            }
+            throw Error;
+          });
       })
       .catch((err) => {
         console.log(err);
