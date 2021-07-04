c0d669f3d4ebb1c08a4b0b52bdf06fd4d55e25bc
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-04 19:47:01 +0000

    Add route to register a user

--- express-back-end/routes/auth.js
+++ express-back-end/routes/auth.js
@@ -3,14 +3,25 @@ const router = require('express').Router();
 module.exports = (db) => {
   // POST: a new user
   router.post('/', (req, res) => {
-    res.json({ ratings: 'Works' });
+    const { username, password, email } = req.body;
+    db('users')
+      .insert({ email, username, password })
+      .then(() => {
+        return res.status(200).json({ success: true });
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ Error: 'Sorry, there was an error during registration' });
+      });
   });
   // GET: a users details for login
+  // RETURNS: A user object
   router.get('/', (req, res) => {
     const { email, password } = req.body;
     db('users')
       .select()
-      .where({ email: email, password: password })
+      .where({ email, password })
       .then((user) => {
         if (user.length > 0) {
           return res.status(200).json({ user });
