f168d8cf731f12add4681428438178da1574c828
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-05 05:03:25 +0000

    Add route to post a new rating to the database

--- express-back-end/routes/ratings.js
+++ express-back-end/routes/ratings.js
@@ -3,7 +3,17 @@ const router = require('express').Router();
 module.exports = (db) => {
   // POST: a new rating
   router.post('/', (req, res) => {
-    res.json({ ratings: 'Works' });
+    const { user_id, game_id, rating } = req.body;
+    db('ratings')
+      .insert({ user_id, game_id, rating })
+      .then(() => {
+        return res.status(200).json({ success: true });
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ Error: 'Sorry, there was an error during rating post!' });
+      });
   });
   // GET: a games total ratings
   router.get('/:id', (req, res) => {
