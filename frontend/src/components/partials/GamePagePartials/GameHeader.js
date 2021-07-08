c4b1c026966af7e7fed3d2f649124d4a8de8f9ed
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-07 23:00:02 -0600

    Add components for adding a game to a user list
    Co-authored-by: Astrid Aguilar <astridcha1x@users.noreply.github.com>

--- frontend/src/components/partials/GamePagePartials/GameHeader.js
+++ frontend/src/components/partials/GamePagePartials/GameHeader.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
+import everyone from '../../../img/esrb_10.png';
+import mature from '../../../img/esrb_mature.png';
+import teen from '../../../img/esrb_teen.svg';
+import pending from '../../../img/esrb_pending.png';
 import { Button, Typography } from 'antd';
 
-const GameHeader = ({ game }) => {
+import ListModal from './ListModal';
+
+const GameHeader = ({ game, user, lists, userCollection, getDetails }) => {
+  const [scoreColour, setScoreColour] = useState('red');
+  const [textColour, setTextColour] = useState('white');
+  const [esrbRating, setEsrbRating] = useState(pending);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (game) {
+      if (game.metacritic_score > 75) {
+        setScoreColour('green');
+      }
+      if (game.metacritic_score > 40 && game.metacritic_score < 75) {
+        setScoreColour('yellow');
+        setTextColour('black');
+      }
+      if (game.esrb_rating !== 'null') {
+        if (game.esrb_rating.toLowerCase().startsWith('t')) {
+          setEsrbRating(teen);
+        }
+        if (game.esrb_rating.toLowerCase().startsWith('m')) {
+          setEsrbRating(mature);
+        }
+        if (game.esrb_rating.toLowerCase().startsWith('e')) {
+          setEsrbRating(everyone);
+        }
+      }
+    }
+  }, [game]);
+
   return (
     <header className='game-header'>
       <div className='game-header-title'>
@@ -18,14 +52,17 @@ const GameHeader = ({ game }) => {
       />
       <div className='header-items'>
         <div className='game-rating-bar'>
-          <Button
-            type='info'
-            size='large'
-            className='game-add-btn'
-            shape='round'
-          >
-            Add To...
-          </Button>
+          {user && (
+            <Button
+              type='info'
+              size='large'
+              className='game-add-btn'
+              shape='round'
+              onClick={() => setIsVisible(true)}
+            >
+              Add To...
+            </Button>
+          )}
           <div style={{ marginLeft: '5rem' }}>
             <h4>
               Health:{' '}
@@ -39,9 +76,33 @@ const GameHeader = ({ game }) => {
                 style={{ width: `${!game ? 0 : game.avg * 4.98}px` }}
               ></div>
             </div>
+            <div className='rating_container'>
+              <h4>Metacritic score: </h4>
+              <div
+                style={{
+                  backgroundColor: `${scoreColour}`,
+                  color: `${textColour}`,
+                }}
+                className='score_container'
+              >
+                <span className='score'>{game && game.metacritic_score}</span>
+              </div>
+              <div className='esrb_container'>
+                <img src={esrbRating} alt='esrb_rating' />
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <ListModal
+        userId={user && user.id}
+        lists={lists}
+        collection={userCollection}
+        visible={isVisible}
+        game={game}
+        getDetails={getDetails}
+        cancel={() => setIsVisible(false)}
+      />
     </header>
   );
 };
---
+++ frontend/src/components/partials/GamePagePartials/ListModal.js
@@ -0,0 +1,122 @@
+import { useState, useEffect } from 'react';
+import getNames from '../../../helpers/get_list_names';
+import Notification from '../Notification';
+import axios from 'axios';
+
+import {
+  Modal,
+  Form,
+  Button,
+  Select,
+  InputNumber,
+  Row,
+  Col,
+  Typography,
+} from 'antd';
+
+const ListModal = ({
+  game,
+  visible,
+  cancel,
+  lists,
+  collection,
+  getDetails,
+  userId,
+}) => {
+  const [insert, setInsert] = useState(null);
+  const [numHours, setNumHours] = useState(0);
+  const [userLists, setUserLists] = useState(null);
+
+  useEffect(() => {
+    if (lists) {
+      setUserLists(getNames(lists, collection));
+    }
+  }, [lists, collection]);
+
+  const submitHandler = (e) => {
+    if (insert) {
+      const id = userLists.filter((item) => item.title === insert)[0].id;
+      const newGame = {
+        num_hours_played: numHours,
+        list_id: id,
+        game_id: game.id,
+      };
+      axios.put(`/api/lists/${id}/${game.id}`, newGame).then(() => {
+        Notification({
+          type: 'success',
+          description: 'Succesfully added Game!',
+          title: 'Game Added!',
+        });
+        getDetails(userId);
+        cancel();
+      });
+    }
+  };
+
+  return (
+    <Modal footer={<> </>} visible={visible} onCancel={cancel}>
+      <h2 style={{ textAlign: 'center' }}>Add to a List or Collection!</h2>
+      <Form name='login' layout='vertical'>
+        <Row justify='center'>
+          <Typography.Title level={5}>
+            Add to one of your Collections! Or search amongst your Lists
+          </Typography.Title>
+        </Row>
+        <Row justify='center' style={{ marginTop: '1rem' }}>
+          <Col span={10}>
+            <Form.Item>
+              <Select
+                showSearch
+                placeholder='Search lists..'
+                optionFilterProp='children'
+                filterOption={(input, option) =>
+                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
+                  0
+                }
+                filterSort={(optionA, optionB) =>
+                  optionA.children
+                    .toLowerCase()
+                    .localeCompare(optionB.children.toLowerCase())
+                }
+                onChange={(e) => {
+                  setInsert(e);
+                }}
+              >
+                {userLists !== null &&
+                  userLists.map((item, index) => (
+                    <Select.Option value={item.title} key={index}>
+                      {item.title}
+                    </Select.Option>
+                  ))}
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row justify='center'>
+          <Col span={8}>
+            <Form.Item label='Number of Hours Played' name='Hours'>
+              <InputNumber min={1} onChange={(value) => setNumHours(value)} />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row justify='center' style={{ marginBottom: '1rem' }}>
+          <Typography.Title level={5}>Insert into: {insert}</Typography.Title>
+        </Row>
+        <Row justify='center'>
+          <Col span={8}>
+            <Button
+              type='primary'
+              onClick={(e) => submitHandler(e)}
+              shape='round'
+              block
+            >
+              Accept
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ListModal;
