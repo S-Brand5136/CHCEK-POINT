0134e54a77e8285a6dfc36c82078aca7a6b377c8
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-11 17:04:54 -0600

    Update table_game seeds

--- express-back-end/db/seeds/04_game.js
+++ express-back-end/db/seeds/04_game.js
@@ -8,7 +8,7 @@ exports.seed = function (knex) {
         { list_id: 9, game_id: 3, num_hours_played: 315 },
         { list_id: 10, game_id: 23, num_hours_played: 150 },
         { list_id: 10, game_id: 75, num_hours_played: 20 },
-        { list_id: 10, game_id: 52, num_hours_played: 45 },
+        { list_id: 10, game_id: 43, num_hours_played: 145 },
         { list_id: 11, game_id: 74, num_hours_played: 0 },
         { list_id: 12, game_id: 50, num_hours_played: 0 },
         { list_id: 13, game_id: 74, num_hours_played: 0 },
