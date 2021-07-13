41188bff43d82b3fbad26ad4f5f8fdc80fc17e5e
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-12 23:28:26 -0600

    Add github link into footer

--- frontend/src/components/partials/Footer.js
+++ frontend/src/components/partials/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Typography } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 
 const Footer = () => {
   return (
@@ -11,6 +12,10 @@ const Footer = () => {
         Created by Brandon, Astrid, and Pavneet during their time at Lighthouse
         Labs
       </Typography.Paragraph>
+      <div style={{ color: 'white', textAlign: 'center' }}>
+        Github Repo:{' '}
+        <GithubOutlined style={{ marginLeft: '1rem', fontSize: '24px' }} />
+      </div>
     </div>
   );
 };
