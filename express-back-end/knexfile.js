8eb9585bed73b03aaed703c214274552950d49af
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-17 14:52:53 -0600

    Add test object in knex config object

--- express-back-end/knexfile.js
+++ express-back-end/knexfile.js
@@ -21,6 +21,25 @@ module.exports = {
     },
   },
 
+  test: {
+    client: 'pg',
+    version: '8.6.0',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'checkpoint_test',
+      port: process.env.DB_PORT,
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true',
