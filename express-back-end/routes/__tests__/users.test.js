c5fc87ea3af16d6008000ec85d96e3ecbc64e209
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-19 14:34:58 -0600

    Refactor code to ensure passing tests

--- express-back-end/routes/__tests__/users.test.js
+++ express-back-end/routes/__tests__/users.test.js
@@ -13,19 +13,36 @@ describe('Testing route /users../', () => {
   test('When sent valid data update a users information', async () => {
     const response = await request(app)
       .put('/api/users/profile')
-      .send({ user_id: 3, username: 'BJ.Shemilt' });
+      .send({
+        user: {
+          id: 3,
+          username: 'JohnDoe',
+          email: 'John.Doe@example.com',
+          platforms: 'PS4, PC',
+          bio: 'John doe',
+          in_game_usernames: 'JohnDozer',
+          pronoun: 'he/him',
+          birthdate: '1999-03-20',
+          timezone: 'UTC-9',
+          discord_username: 'John Dozer',
+        },
+      });
     expect(response.statusCode).toBe(200);
     expect(response.body.user).not.toBe(undefined);
-    expect(response.body.user.username).toBe('BJ.Shemilt');
+    expect(response.body.user.username).toBe('JohnDoe');
+    expect(response.body.user.email).toBe('John.Doe@example.com');
+    expect(response.body.user.bio).toBe('John doe');
+    expect(response.body.user.in_game_usernames).toBe('JohnDozer');
+    expect(response.body.user.birthdate).toBe('1999-03-20T00:00:00.000Z');
   });
 
   test('When sent invalid update data, return a 400 error with response ', async () => {
     const response = await request(app)
       .put('/api/users/profile')
-      .send({ user_id: null, discord_username: 'johnDoe' });
+      .send({ user: { user_id: null, discord_username: 'johnDoe' } });
     expect(response.statusCode).toBe(400);
     expect(response.body.user).toBe(undefined);
-    expect(response.error).toBe(
+    expect(response.body.error).toBe(
       'Sorry, there was a problem updating your information! Try again.'
     );
   });
@@ -34,27 +51,23 @@ describe('Testing route /users../', () => {
   test('Return user stats on get request', async () => {
     const response = await request(app).get('/api/users/3/stats');
     expect(response.statusCode).toBe(200);
-    expect(response.userStats.length).toBe(3);
-  });
-
-  test('Should return a 404 when an invalid userId is sent', async () => {
-    const response = await request(app).get('/api/users/-1/stats');
-    expect(response.statusCode).toBe(404);
-    expect(response.error).toBe('Sorry, that users stats could not be found!');
+    expect(response.body.userStats.length).toBe(3);
   });
 
   // ROUTE: /api/users/
   test('Should return all users in database', async () => {
     const response = await request(app).get('/api/users');
-    expect(repsonse.statusCode).toBe(200);
-    expect(response.users.length).toBe(3);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.users.length).toBe(3);
   });
 
   test('Should return a 404 when an error occurs retrieving all users', async () => {
-    db.migrate.rollback();
+    await db.migrate.rollback();
     const response = await request(app).get('/api/users');
     expect(response.statusCode).toBe(404);
-    expect(response.error).toBe('Sorry, there was a problem retrieving users!');
+    expect(response.body.error).toBe(
+      'Sorry, there was a problem retrieving users!'
+    );
   });
 
   afterAll((done) => {
