06b97f42e2fc9566a7ea0b1daf34b055c07a2f26
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-19 12:18:05 -0600

    Write failing tests for auth route

--- express-back-end/routes/__tests__/auth.test.js
+++ express-back-end/routes/__tests__/auth.test.js
@@ -1,3 +1,5 @@
+const request = require('supertest');
+const app = require('../../app');
 const db = require('../../db-config');
 
 beforeAll(async () => {
@@ -6,7 +8,54 @@ beforeAll(async () => {
   await db.seed.run();
 });
 
-test('Log in to account', async () => {
-  let games = null;
-  expect(games).toEqual(null);
+describe('Testing /auth route...', () => {
+  // Route /api/auth/register
+  test('When given valid data, create a new user', async () => {
+    const response = await request(app).post('api/auth/register').send({
+      username: 'example_user',
+      email: 'example@example.com',
+      password: '12345',
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.user.username).toBe('example_user');
+    expect(response.user.email).toBe('example@example.com');
+    expect(response.user.password).toBe('12345');
+    expect(response.success).toBe(true);
+  });
+
+  test('When invalid data is given, return 400 code with error message', async () => {
+    const response = await request(app).post('api/auth/register').send({
+      username: null,
+      email: undefined,
+      password: '12345',
+    });
+    expect(response.statusCode).toBe(401);
+    expect(response.error).toBe(
+      'There was an error during registration! Try again'
+    );
+  });
+
+  // Route /api/auth/login
+  test('When given valid user credentials, login', async () => {
+    const response = await request(app).post('/api/auth/loign').send({
+      email: 'Brandon.Shemilt@example.com',
+      password: '12345',
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.user.email).toBe('Brandon.Shemilt@example.com');
+    expect(response.user.userName).toBe('Brandon');
+  });
+
+  test('When given invalid user credentials, return 401 with error message', async () => {
+    const response = await request(app).post('/api/auth/loign').send({
+      email: null,
+      password: undefined,
+    });
+    expect(response.statusCode).toBe(401);
+    expect(response.error).toBe('Invalid Credentials');
+  });
+
+  afterAll((done) => {
+    done();
+  });
 });
